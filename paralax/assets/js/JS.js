78840c333d661b37f7a55a8b4d594c16bff602a9
Sergei-Triyanov <82256719+Sergei-Triyanov@users.noreply.github.com>
2021-06-29 11:57:09 +0300

    Add files via upload

---
+++ paralax/assets/js/JS.js
@@ -0,0 +1,6 @@
+// паралах эффект для текста
+const text = document.getElementById('text');
+const parallaxInstance = new Parallax(text);
+// паралах эффект для изображений
+const img = document.getElementById('img');
+const parallaxInstanceImg= new Parallax(img);
