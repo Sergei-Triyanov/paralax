e95b1159ec16f4aa404a79a0a620c763c5e517ba
Sergei-Triyanov <82256719+Sergei-Triyanov@users.noreply.github.com>
2021-07-19 17:48:35 +0300

    Delete paralax directory

--- paralax/assets/js/JS.js
+++
@@ -1,6 +0,0 @@
-// паралах эффект для текста
-const text = document.getElementById('text');
-const parallaxInstance = new Parallax(text);
-// паралах эффект для изображений
-const img = document.getElementById('img');
-const parallaxInstanceImg= new Parallax(img);
